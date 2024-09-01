e8a9a6c84227335baeedbda00704ee29278dc9ac
Stefanfromhome <stefrobthings@gmail.com>
2024-08-31 21:09:19 -0400

    fix server ln 69-74

--- idk.py
+++ idk.py
@@ -1,12 +1,27 @@
 import os
+import smtplib
+
 
 #tinker holds tkinter library
 import tkinter as tinker
-
+program_email = "gridmanager215@gmail.com"
 # Prompt user to choose between creating a new folder or using an existing one
 create = input("1.) Create a folder to store these passwords, 2.) Store in existing folder, 3.)Open Existing Folder: ")
 
-if create == "3":
+
+
+
+if create == "1":
+    # Create a new folder
+    folder_name = input("Make a name for the folder you can find this all in: ")
+    os.makedirs(folder_name, exist_ok=True)
+    print(f"If you go looking for these they will be inside of '{folder_name}'")
+    
+elif create == "2":
+    # Use an existing folder
+    folder_name = input("Enter name of existing folder: ")
+
+elif create == "3":
     #Open Existing Folder and make GUI using tkinter
     root  = tinker.Tk()
 
@@ -24,16 +39,7 @@ if create == "3":
     root.mainloop()
 
 
-if create == "1":
-    # Create a new folder
-    folder_name = input("Make a name for the folder you can find this all in: ")
-    os.makedirs(folder_name, exist_ok=True)
-    print(f"If you go looking for these they will be inside of '{folder_name}'")
-elif create == "2":
-    # Use an existing folder
-    folder_name = input("Enter name of existing folder: ")
-
-#3= Inside password text implement option for 2Factor Authentication 
+ #3= Inside password text implement option for 2Factor Authentication 
 #4 = Forgot password - Using a librabry can send email containing things
 
 
@@ -41,10 +47,33 @@ elif create == "2":
 specify = input("Name of password designation (basically what is it for): ")
 password = input("Password: ")
 
+
+
 # Combine the designation and password
 combine = specify + ": " + password
 file_name = "passwords.txt"
 file_path = os.path.join(folder_name, file_name)
+
+#send passwords to email
+
+receiver_email = input("Add email for backup: ")
+
+
+subject ="Password Manager"  
+
+#Get passwords from txt file
+message_passwords = "Password Set:  "
+
+message = f"Subject: {subject} \n\n{message_passwords}
+
+server = smtplib.SMTP("smtp.gmail.com", 587)
+server.starttls() 
+
+server.login(program_email, "fbbyaileaqdabnul")
+
+server.sendmail(program_email,receiver_email, message)
+
+
 
 # Write the combined string to the file
 with open(file_path, 'a') as file:
